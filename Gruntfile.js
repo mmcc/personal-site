21287c54ca83c1d74ab0f786af3f2be8b961a440
Matt McClure <matt@htbx.net>
2015-01-25 22:22:24 -0800

    added harp gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    watch: {
+      scripts: {
+        files: ['./src-js/**/*'],
+        tasks: ['browserify'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      dev: {
+        tasks: ['watch', 'harp']
+      }
+    },
+    harp: {
+      server: {
+        server: true,
+        source: 'public'
+      },
+      dist: {
+        source: 'public',
+        dest: 'www'
+      }
+    },
+    browserify: {
+      dist: {
+        files: {
+          'public/js/bundle.js': ['src-js/app.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-harp');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  // Default task(s).
+  grunt.registerTask('default', ['browserify']);
+  grunt.registerTask('build', ['browserify', 'harp:dist']);
+  grunt.registerTask('dev', ['concurrent']);
+};
